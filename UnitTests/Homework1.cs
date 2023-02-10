ac4447c79a673d83fd465c15addda988292b97fe
Monika Balandyte <monika.balandyte@companial.com>
2023-02-10 15:38:03 +0200

    Add homework tasts

--- UnitTests/Homework1.cs
+++ UnitTests/Homework1.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 using NUnit.Framework;
 
 namespace UnitTests
@@ -11,7 +8,7 @@ namespace UnitTests
     {
         [Test]
         public void CheckRemainder995DividedBy3()
-        { 
+        {
             int expectedResult = 0;
 
             int actualResult = 995 % 3;
@@ -19,5 +16,41 @@ namespace UnitTests
             Assert.AreEqual(expectedResult, actualResult);
         }
 
+        [Test]
+        public void CheckIfTodayIsWednesday()
+        {
+            DateTime currentDate = DateTime.Now;
+
+            DayOfWeek currentWeekday = currentDate.DayOfWeek;
+
+            Assert.AreEqual(DayOfWeek.Wednesday, currentWeekday);
+        }
+
+        [Test]
+        public void CheckIfNowIs13hour()
+        {
+            DateTime currentDate = DateTime.Now;
+
+            int currentHour = currentDate.Hour;
+
+            Assert.AreEqual(13, currentHour);
+
+        }
+        [Test]
+        public void CheckIfFourEvenNumbersAreBetween1and10Inclusive()
+        {
+       
+            int count = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Assert.AreEqual(4, count);
+        }
     }
 }
+
