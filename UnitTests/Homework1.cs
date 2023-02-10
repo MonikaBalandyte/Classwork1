92146166ea1d241ac44bf64fbf98289b423c1adf
Monika Balandyte <monika.balandyte@companial.com>
2023-02-10 13:57:07 +0200

    Add solution to first excercise

---
+++ UnitTests/Homework1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    internal class Homework1
+    {
+        [Test]
+        public void CheckRemainder995DividedBy3()
+        { 
+            int expectedResult = 0;
+
+            int actualResult = 995 % 3;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+    }
+}
