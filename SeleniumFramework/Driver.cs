d9cb12de9d59e9a302571f331e67d3ad59f9ee6c
Monika Balandyte <monika.balandyte@companial.com>
2023-03-22 14:15:58 +0200

    Add driver class

--- SeleniumFramework/Driver.cs
+++ SeleniumFramework/Driver.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumFramework
 {
-    internal class Driver
+   
+
+    public class Driver
     {
+        private static IWebDriver driver;
+
+        public static void SetupDriver()
+        { 
+            driver = new ChromeDriver();
+        }
+
+        public static IWebDriver GetDriver() 
+        {
+            return driver;
+        }
+
+        public static void OpenURL(string url) 
+        {
+            //su refresh
+            driver.Url = url;
+            //be refresh, išlaikom veiksmus, kurie galėjo būti atlikti svetainėje
+           // driver.Navigate().GoToUrl(url);
+        }
+
+        public static void CloseDriver() 
+        {
+            driver.Quit();
+        }
     }
 }
