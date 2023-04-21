f5e48db8fca59f1110a5efd6a2d78231c7832745
Monika Balandyte <monika.balandyte@companial.com>
2023-04-21 14:24:05 +0300

    Add test for single check box

---
+++ Selenium1Tests/SeleniumEasy/CheckBoxDemoTests.cs
@@ -0,0 +1,39 @@
+﻿
+
+using NUnit.Framework;
+using SeleniumFramework;
+using SeleniumFramework.Pages.SeleniumEasy;
+
+namespace Selenium1Tests.SeleniumEasy
+{
+    internal class CheckBoxDemoTests
+    {
+        [SetUp]
+
+        public void SetUp()
+        {
+            Driver.SetupDriver();
+            CheckBoxDemo.Open();
+        }
+        [Test]
+
+        public void SingleCheckbox()
+        {
+            string expectedMessage = "Success - Check box is checked";
+
+            CheckBoxDemo.ClickSingleCheckbox();
+            string actualResult = CheckBoxDemo.GetSingleCheckBoxSuccessMessage();
+
+            Assert.AreEqual(expectedMessage, actualResult);
+        
+        }
+
+        [TearDown]
+
+        public void TearDown()
+
+        {
+            Driver.CloseDriver();
+        }
+    }
+}
---
+++ SeleniumFramework/Pages/SeleniumEasy/CheckBoxDemo.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace SeleniumFramework.Pages.SeleniumEasy
+{
+    public class CheckBoxDemo
+    {
+        private static string inputSingleCheckbox = "//*[@id='isAgeSelected']";
+        private static string fieldSingleCheckboxSuccessMessage = "//*[@id='txtAge']";
+
+        public static void Open()
+        {
+            Driver.OpenURL("http://demo.seleniumeasy.com/basic-checkbox-demo.html");
+        }
+        public static void ClickSingleCheckbox()
+        {
+            Common.ClickElement(inputSingleCheckbox);
+        }
+
+        public static string GetSingleCheckBoxSuccessMessage()
+        {
+            return Common.GetElementText(fieldSingleCheckboxSuccessMessage);
+
+        }
+    }
+}
